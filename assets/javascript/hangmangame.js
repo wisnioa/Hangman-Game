c0cf64ce7fe7b294b9f04b6d3de0511e42ba8d72
unknown <amanda.wisniowski@gmail.com>
2017-08-16 18:53:08 -0500

    JS needs some work! Thank you

---
+++ assets/javascript/hangmangame.js
@@ -0,0 +1,29 @@
+var answers = ["ocean", "shell", "mermaid", "waves", "riptide", "seagull"];
+var userGuess;
+var winsCount;
+
+//find a way to select a random word from answers
+
+
+function() {
+  
+ var randomWord = answers[Math.floor(Math.random() * answers.length)];
+ 	
+ 	document.getElementById("userGuess").innerHTML;
+		}
+
+
+//this will be an empty array this will carry all the _ for the amount of dashes in our word
+var randomWordDash = [];
+
+//empty underscores
+//answers.length will always give you the total
+// for (i = 0; i < answers.length; i++) {
+    for (i = 0; i < answers[1].length; i++) {
+        //this will push an underscore to our empty array for the length of the selected RANDOM word.
+    randomWordDash.push("_");
+
+}
+var html = "<h1>Word is this many characters: " + randomWordDash+"</h1>";
+console.log(html);
+document.getElementById("gameWord").innerHTML = html;
