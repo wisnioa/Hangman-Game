8bd1ab59f5620e274dd2279f18274f0c2cf32449
unknown <amanda.wisniowski@gmail.com>
2017-08-17 23:09:49 -0500

    hard time pushing to github pages?

--- assets/javascript/hangmangame.js
+++ assets/javascript/hangmangame.js
@@ -1,29 +1,61 @@
 var answers = ["ocean", "shell", "mermaid", "waves", "riptide", "seagull"];
 var userGuess;
-var winsCount;
+var winsCount =0;
+var numGuess; 
+var randomWord;
 
-//find a way to select a random word from answers
+
+document.onkeyup = function(event) {
+var userGuess = event.key;
+document.getElementById("guessed").innerHTML = userGuess;
+}
+
+
 
 
 function choose() {
   
- var randomWord = answers[Math.floor(Math.random() * answers.length)];
+ var randomWord = answers[Math.floor(Math.random() * answers.length++)];
  	
  	document.getElementById("").innerHTML;
 		}
 
 
-//this will be an empty array this will carry all the _ for the amount of dashes in our word
 var randomWordDash = [];
 
-//empty underscores
-//answers.length will always give you the total
-// for (i = 0; i < answers.length; i++) {
-    for (i = 0; i < answers[1].length; i++) {
-        //this will push an underscore to our empty array for the length of the selected RANDOM word.
-    randomWordDash.push("_");
 
+    for (i = 0; i < answers[1].length; i++) {
+           randomWordDash.push(" _ ");
+
+
+
+
+
+
+function increaseWins(){
+winsCount++;
+document.getElementById('count').innerHTML= +i;
 }
+
+
+
+
 var html = "<h3> Current Word \n" + randomWordDash+"</h3>";
 console.log(html);
 document.getElementById("gameWord").innerHTML = html;
+
+
+userGuess = "<p> Letters Already Guessed \n </p>";
+console.log(userGuess);
+document.getElementById("guessed").innerHTML = userGuess;
+
+
+numGuess = "<p> Guesses Remaining \n </p>"; 
+console.log(numGuess);
+document.getElementById("toGuess").innerHTML = numGuess; 
+
+
+
+winsCount = "<p> Wins \n </p>"; 
+console.log(winsCount);
+document.getElementById("count").innerHTML = winsCount;
